790212d43f4fa7d369eb176e591a78949dc0e397
Kaung-Nyo <st125066@ait.asia>
2024-10-09 05:16:28 +0700

    test

--- code/test_app_callbacks.py
+++ code/test_app_callbacks.py
@@ -9,8 +9,8 @@ sample = np.array([[ 0.92753623,  0.,  1.,  0. ,  1.,1., -0.41967976, -0.4969184
 
 def test_model_output_shape():
     output = model.predict(sample)
-    assert output.shape == (1,-1), f"Expecting the shape to be (1,1) but got {output.shape=}"
+    assert output.shape == (1,), f"Expecting the shape to be (1,) but got {output.shape=}"
 
 def test_model_coeff_shape():
     output = model.W
-    assert output.shape == (8,4), f"Expecting the shape to be (1,2) but got {output.shape=}"
+    assert output.shape == (8,4), f"Expecting the shape to be (8,4) but got {output.shape=}"
